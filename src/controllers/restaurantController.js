fc36d4f90a7f90d2517a66ca15bc6e7208f68129
Nirmala <nirmalawilujeng@gmail.com>
2026-05-15 17:15:21 +0700

    add create restaurant and get restaurant by username functionality

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -18,6 +18,45 @@ const restaurantController = {
             });
         }
     },
+    createRestaurant: async (req, res) => {
+        const { username, display_name, url_img, password, phone } = req.body;
+        if(!username || !display_name || !url_img || !password || !phone) return res.status(400).json({
+                success: false,
+                message: 'Username, Display Name, Profile Image, Password, and Phone required',
+                data: null,
+            });
+        try {
+            const restaurant = await restoRepo.createRestaurant(username, display_name, url_img, password, phone);
+            res.status(200).json({
+                success: true,
+                message: 'Successfully create new restaurant',
+                data: restaurant,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success:false,
+                message: 'Internal Server Error',
+                data: error.message,
+            });
+        }
+    },
+    getRestaurantByUsername: async (req, res) => {
+        try {
+            const { username }  = req.params;
+            const restaurant = await restoRepo.getrestaurantByUsername(username);
+            res.status(200).json({
+                success: true,
+                message: `Successfully retrieve ${username}`,
+                data: result,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success:false,
+                message: 'Internal Server Error',
+                data: error.message,
+            });
+        }
+    }
 };
 
 
